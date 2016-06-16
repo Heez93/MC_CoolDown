1db47bf8e06ea781b8d65a7ab985bd7610dbd307
Heez93 <hpmc93@gmail.com>
2016-06-16 19:18:02 +0200

    Standard Version

--- MC_CoolDown.lua
+++ MC_CoolDown.lua
@@ -1,29 +1,27 @@
-local skill_01 = CreateFrame("Frame", "skill_01", UIParent)
-skill_01:SetWidth(64)
-skill_01:SetHeight(64)
-skill_01:ClearAllPoints()
-skill_01:SetPoint("CENTER", 0, 0)
-skill_01:RegisterEvent("ACTIONBAR_UPDATE_STATE")
-skill_01:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN")
-local skill_01_texture = skill_01:CreateTexture("skill_01_texture")
-skill_01_texture:SetTexture(GetActionTexture(1))
-skill_01_texture:SetAllPoints(skill_01)
-skill_01_texture:Hide()
-
+local myFrame = CreateFrame("Frame", "myFrame", UIParent)
+myFrame:SetWidth(64)
+myFrame:SetHeight(64)
+myFrame:ClearAllPoints()
+myFrame:SetPoint("CENTER", 0, 0)
+myFrame:RegisterEvent("SPELL_UPDATE_USABLE")
+local texture = myFrame:CreateTexture("CDTexture")
+texture:SetTexture(GetActionTexture(1))
+texture:SetAllPoints(myFrame)
+texture:Hide()
 local i = 1
 
 function displayCD()
 	local start, duration, enable = GetActionCooldown(1)
 	if(start > 0 and i == 1) then
-		skill_01_texture:Show()
+		texture:Show()
 		print("This skill is on Cooldown")
 		i = i + 1
 	end
 	if (start == 0) then
-		skill_01_texture:Hide()
+		texture:Hide()
 		print("This skill is available")
 		i = 1
 	end
 end
 
-skill_01:SetScript("OnEvent", displayCD)
+myFrame:SetScript("OnEvent", displayCD)
